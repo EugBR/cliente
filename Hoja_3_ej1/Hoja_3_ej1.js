2c0c36886ddc97691e8275a00ef6c5389f55cb98
Eugenio <eugeniobernues@gmail.com>
2021-10-01 17:18:08 +0200

    fin clase 1/10/2021

---
+++ Hoja_3_ej1/Hoja_3_ej1.js
@@ -0,0 +1,16 @@
+
+var palabras = [true,5,false,"hola","adios",2];
+
+
+if(palabras[2]>palabras[3]){
+    document.writeln("El tercer componente es más grande<br>");
+}else{
+    document.writeln("El cuarto componente es más gramde<br>");
+}
+
+if(palabras[0] < palabras[1] || palabras[2] < palabras[1]){
+    document.writeln("El primer o tercer componente son mayores que el segundo<br>");
+}
+
+
+
